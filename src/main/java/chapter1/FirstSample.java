0d476ca92824cbf4b7e4e5f83a2e9ddafe117d4f
Hyunwoo Cho <barefoot1423@gmail.com>
2020-04-18 23:27:17 -0400

    add a comment

--- src/main/java/chapter1/FirstSample.java
+++ src/main/java/chapter1/FirstSample.java
@@ -2,6 +2,6 @@ package chapter1;
 
 public class FirstSample {
     public static void main(String[] args) {
-        System.out.println("We will not use 'Hello, World!");
+        System.out.println("We will not use 'Hello, World!"); // is this too cute?
     }
 }
