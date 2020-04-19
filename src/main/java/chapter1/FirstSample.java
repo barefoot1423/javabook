fda4085fd80a203b7812ee707b63e5e2bd18e927
Hyunwoo Cho <barefoot1423@gmail.com>
2020-04-18 23:39:59 -0400

    add another type of comments

--- src/main/java/chapter1/FirstSample.java
+++ src/main/java/chapter1/FirstSample.java
@@ -1,5 +1,8 @@
 package chapter1;
 
+/**
+ * This is the first sample program in Core Java chapter 3
+ */
 public class FirstSample {
     public static void main(String[] args) {
         System.out.println("We will not use 'Hello, World!"); // is this too cute?
