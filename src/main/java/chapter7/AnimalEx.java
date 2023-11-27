fc9baa628a99c8cfbd393b033d406f9dc57d88c3
Hyunwoo Cho <barefoot1423@gmail.com>
2023-11-27 16:10:15 -0500

    commit abstract

---
+++ src/main/java/chapter7/AnimalEx.java
@@ -0,0 +1,11 @@
+package chapter7;
+
+public class AnimalEx {
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+
+        cat.sound();
+    }
+
+}
---
+++ src/main/java/chapter7/Cat.java
@@ -0,0 +1,9 @@
+package chapter7;
+
+public class Cat extends Animal{
+
+    public void sound() {
+        System.out.println("Meeyao");
+    }
+
+}
