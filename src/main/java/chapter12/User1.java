c1641e6e2c301e4bb024405b234382176d44f94a
Hyunwoo Cho <barefoot1423@gmail.com>
2023-12-03 16:34:06 -0500

    commit thread safe test

---
+++ src/main/java/chapter12/User1.java
@@ -0,0 +1,21 @@
+package chapter12;
+
+public class User1 extends Thread {
+
+    private Calculator calculator;
+
+    public Calculator getCalculator() {
+        return calculator;
+    }
+
+    public void setCalculator(Calculator calculator) {
+        this.setName("User1");
+        this.calculator = calculator;
+    }
+
+    public void run() {
+        calculator.setMemory(100);
+    }
+
+
+}
